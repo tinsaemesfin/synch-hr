02ff37514bbdc4c664e9c914558a794921d3a880
tinsaemesfin <qmesfin@gmail.com>
2023-09-20 16:57:08 +0300

    changed cookiesStore

--- app/(tenant)/employee/[employeeId]/page.tsx
+++ app/(tenant)/employee/[employeeId]/page.tsx
@@ -20,15 +20,16 @@ import Attendance from "./(tabs)/(attendance)/attendance";
 const Page = async ({ params }: { params: { employeeId: string } }) => {
   let employee: IEmployee | null = null;
   let token :string|undefined ;
+  const cookieStore =  cookies()
+  const rawToken = cookieStore.get('next-auth.session-token')
+  token = rawToken?.value
+  if(!token) return null
 
 
   
   try {
     await dbConnect();
-    const cookieStore = cookies()
-  const rawToken = cookieStore.get('next-auth.session-token')
-  token = rawToken?.value
-  if(!token) return null
+   
 
     // FIXME :  JUST FOR MAKING EASY FOR CHECKING
     const employeeResponse : AxiosResponse = await axios.get(`${process.env.BASE_URL}/api/employee/${params.employeeId}`,{
