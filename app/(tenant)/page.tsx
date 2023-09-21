49dba8ce76dcfc87259eba7ce0ad191ee1500a1f
tinsaemesfin <qmesfin@gmail.com>
2023-09-21 11:26:29 +0300

    check unautenticated

--- app/(tenant)/page.tsx
+++ app/(tenant)/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useSession } from "next-auth/react"
-import moment from "moment-timezone";
+import { redirect } from "next/navigation";
 
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
 
 
 if(status === 'loading') return <h1> Loading ... </h1>
+if(status === 'unauthenticated') redirect("/signin");
   return (
    <h1> DashBoard 
     Home Page {JSON.stringify(data)} {
