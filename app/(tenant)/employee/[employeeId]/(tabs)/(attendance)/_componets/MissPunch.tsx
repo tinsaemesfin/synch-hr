c3f33f086f81ff3de61dadfb884d08a28e291df1
tinsaemesfin <qmesfin@gmail.com>
2023-10-02 09:30:43 +0300

    comment all misspunch

--- app/(tenant)/employee/[employeeId]/(tabs)/(attendance)/_componets/MissPunch.tsx
+++ app/(tenant)/employee/[employeeId]/(tabs)/(attendance)/_componets/MissPunch.tsx
@@ -1,29 +1,29 @@
-import { TableCell } from '@/components/ui/table'
-import React from 'react'
-interface props{
-    index:number,
-    penaltyMoney:number
-}
+// import { TableCell } from '@/components/ui/table'
+// import React from 'react'
+// interface props{
+//     index:number,
+//     penaltyMoney:number
+// }
 
-const MissPunch:React.FC<props> = ({index,penaltyMoney}) => {
-  return (
-    <TableCell key={index}>
-              <span className="text-blue-600">
-                {todaysAttendanceTimes[0].toDateString()}{" "}
-              </span>{" "}
-              <br />
-              <span className="text-red-700">miss</span>
-              <span className="text-red-700">
-                {" "}
-                -
-                {PenaltyMoneyCalculator(
-                  activeContract.grossSalary.$numberDecimal,
-                  1
-                )}{" "}
-                Birr
-              </span>{" "}
-            </TableCell>
-  )
-}
+// const MissPunch:React.FC<props> = ({index,penaltyMoney}) => {
+//   return (
+//     <TableCell key={index}>
+//               <span className="text-blue-600">
+//                 {todaysAttendanceTimes[0].toDateString()}{" "}
+//               </span>{" "}
+//               <br />
+//               <span className="text-red-700">miss</span>
+//               <span className="text-red-700">
+//                 {" "}
+//                 -
+//                 {PenaltyMoneyCalculator(
+//                   activeContract.grossSalary.$numberDecimal,
+//                   1
+//                 )}{" "}
+//                 Birr
+//               </span>{" "}
+//             </TableCell>
+//   )
+// }
 
-export default MissPunch
+// export default MissPunch
